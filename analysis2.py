e7d0a64fc71a3c241ada8574e1e3b041ca6657e0
nimrodzeus83-de <nimrodzeus83@gmail.com>
2026-06-12 16:42:22 +0800

    Update analysis2.py

--- analysis2.py
+++ analysis2.py
@@ -1,9 +1,69 @@
 import pandas as pd 
 import matplotlib.pyplot as plt 
 
+df = pd.read_csv(
+    r"C:\Users\MySQLAdmin\Documents\Transactions.csv",
+    encoding="cp1252"
+    )
+
+df.columns = (
+    df.columns 
+    .str.strip()
+    .str.lower()
+    .str.replace(" ", "_")
+    .str.replace("-", "_")
+    .str.replace("(", "")
+    .str.replace(")","")
+)
+
+for cols in df.select_dtypes(include="object"):
+    df[cols] = df[cols].astype(str).str.strip()
+    
+df = df.replace(["", "-", " - "],pd.NA)
+
+money_cols = [
+    "price", "package_", "gross",
+    "disc_amt", "sales", "dep"
+]
+
+for cols in money_cols:
+    df[cols] = (
+       df[cols]
+       .astype(str)
+       .str.replace(",", "",regex=False) 
+    )
+    df[cols] = pd.to_numeric(df[cols], errors="coerce")
+
+df["customer_rating"] = pd.to_numeric(
+    df["customer_rating"], 
+    errors="coerce"
+)
+
+date_cols = ["arrival_date", "depature_date"]
+
+for cols in date_cols:
+    df[cols] = pd.to_datetime(
+        df[cols],
+        errors = "coerce",
+        dayfirst=True
+    )
+    
+for cols in date_cols:
+    df[cols] = df[cols].dt.strftime("%Y-%m-%d")
+    
+df["membership"] = df["membership"].fillna("Unknown")
+
+df = df.drop_duplicates()
+
+df.to_csv("new_data.csv", index=False)
+
+print("Data cleaned successfully")
+
+
+
+
+
 dff = pd.read_csv("new_data.csv")
-
-
 print(dff[["hotel_name","membership"]])
 
 membership_counts = pd.crosstab(dff["hotel_name"], dff["membership"])
